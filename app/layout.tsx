e022276d198635e97d7d2c866b0a8ab0e0a303b8
Sam Garson <samtgarson@gmail.com>
2022-12-27 23:35:31 +0000

    Bring back app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+import { AuthProvider } from '@/components/auth/auth-provider'
 import { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
       <head />
-      <body>{children}</body>
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   )
 }
