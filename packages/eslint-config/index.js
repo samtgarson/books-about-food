b4ae8b27422cc51883100839b7755742ace23df5
Sam Garson <samtgarson@gmail.com>
2022-12-28 12:46:51 +0000

    Shared eslint package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@next/next/recommended",
+    "@samtgarson/eslint-config",
+    "@samtgarson/eslint-config/typescript",
+    "@samtgarson/eslint-config/react",
+    "plugin:react/jsx-runtime"
+  ]
+}
