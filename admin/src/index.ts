2de6a5a1951430e6cd51d326d1b16bd7ec4af1bf
Sam Garson <samtgarson@gmail.com>
2022-12-28 12:34:34 +0000

    Install forest admin

---
+++ admin/src/index.ts
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config() // eslint-disable-line
+}
+
+import { createAgent } from '@forestadmin/agent'
+import { createSqlDataSource } from '@forestadmin/datasource-sql'
+import express from 'express'
+import { getEnv } from './utils/get-env'
+
+// Create your Forest Admin agent
+const agent = createAgent({
+  // These process.env variables should be provided in the onboarding
+  authSecret: getEnv('FOREST_AUTH_SECRET'),
+  envSecret: getEnv('FOREST_ENV_SECRET'),
+  isProduction: process.env.NODE_ENV === 'production'
+})
+
+const app = express()
+
+agent
+  .addDataSource(createSqlDataSource(getEnv('DATABASE_URL')))
+  .mountOnExpress(app)
+  .start()
+
+const port = process.env.PORT || 3001
+if (process.env.NODE_ENV === 'development') {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+}
+
+export default app
