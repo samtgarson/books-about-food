5b02345c06494d36e6fb419bd5181a91cc4d0b63
Sam Garson <samtgarson@gmail.com>
2022-12-28 19:41:13 +0000

    Limit admin db connections

--- admin/index.ts
+++ admin/index.ts
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV !== 'production') {
 import { createAgent } from '@forestadmin/agent'
 import { createSqlDataSource } from '@forestadmin/datasource-sql'
 import { getEnv } from 'shared'
+import { parse } from 'pg-connection-string'
+import pg from 'pg'
 
 // Create your Forest Admin agent
 const agent = createAgent({
@@ -14,7 +16,20 @@ const agent = createAgent({
   isProduction: process.env.NODE_ENV === 'production'
 })
 
-agent.addDataSource(createSqlDataSource(getEnv('DATABASE_URL'))).start()
+const parsed = parse(getEnv('DATABASE_URL'))
+const datasource = createSqlDataSource({
+  username: parsed.user ?? undefined,
+  password: parsed.password ?? undefined,
+  host: parsed.host ?? undefined,
+  port: parsed.port ? parseInt(parsed.port) : undefined,
+  database: parsed.database ?? undefined,
+  dialect: 'postgres',
+  dialectModule: pg,
+  pool: {
+    max: 1
+  }
+})
+agent.addDataSource(datasource).start()
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001
 agent.mountOnStandaloneServer(port)
