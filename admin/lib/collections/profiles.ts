15ef69bd321ddae9427f22d6cd441dadb04866f3
Sam Garson <samtgarson@gmail.com>
2023-01-02 20:02:42 +0000

    Dry up formatting scripts

--- admin/lib/collections/profiles.ts
+++
@@ -1,7 +0,0 @@
-import { CollectionCustomizer } from '@forestadmin/agent'
-import { uploadImage } from 'lib/utils/image-utils'
-import { Schema } from '../../.schema/types'
-
-export const customiseProfiles = (
-  collection: CollectionCustomizer<Schema, 'profiles'>
-) => {}
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@samtgarson/eslint-config'],
+  env: {
+    es6: true
+  },
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
@@ -8,6 +11,13 @@ module.exports = {
     {
       files: ['**/*.tsx'],
       extends: ['@samtgarson/eslint-config/react', 'plugin:react/jsx-runtime']
+    },
+    {
+      files: ['web/**/*.ts', 'web/**/*.ts'],
+      extends: ['next/core-web-vitals', 'plugin:@next/next/recommended']
     }
-  ]
+  ],
+  rules: {
+    '@next/next/no-html-link-for-pages': ['error', 'web']
+  }
 }
