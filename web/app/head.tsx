41a671e4e019857c5df5396925cdafafea6ac1c6
Sam Garson <samtgarson@gmail.com>
2023-02-19 19:56:27 +0000

    Adds favicon and app manifest, closes BAF-84

--- web/app/head.tsx
+++ web/app/head.tsx
@@ -1,8 +1,18 @@
 export default function Head() {
   return (
     <>
-      <title></title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <title>Books About Food</title>
+
+      <link rel="manifest" href="/manifest.json" />
+      <link href="/images/icons/icon-72x72.png" rel="icon" type="image/png" />
     </>
   )
 }
