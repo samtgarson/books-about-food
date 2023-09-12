848dd0a67183612553a839770ebd9f9b90c7cf7c
Sam Garson <samtgarson@gmail.com>
2023-09-12 11:13:56 +0100

    Refactor profile page into component

--- web/app/(main)/authors/[slug]/page.tsx
+++ web/app/(main)/authors/[slug]/page.tsx
@@ -1,19 +1,10 @@
-// import { fetchProfiles } from 'src/services/profiles/fetch-profiles'
-
-import { createProfilePage } from 'src/components/pages/profile-page'
+import { ProfilePage } from 'src/components/pages/profile-page'
+import { PageProps } from 'src/components/types'
 
 export * from 'app/default-static-config'
 
-// export const generateStaticParams = async () => {
-//   const { profiles } = await fetchProfiles.call({
-//     perPage: 0,
-//     onlyAuthors: true,
-//     sort: 'trending'
-//   })
-//
-//   return profiles.map((profile) => ({
-//     slug: profile.slug
-//   }))
-// }
-
-export default createProfilePage('authors')
+export default function PeoplePage({
+  params: { slug }
+}: PageProps<{ slug: string }>) {
+  return <ProfilePage segment="authors" slug={slug} />
+}
--- web/app/(main)/people/[slug]/page.tsx
+++ web/app/(main)/people/[slug]/page.tsx
@@ -1,5 +1,10 @@
-import { createProfilePage } from 'src/components/pages/profile-page'
+import { ProfilePage } from 'src/components/pages/profile-page'
+import { PageProps } from 'src/components/types'
 
 export * from 'app/default-static-config'
 
-export default createProfilePage('people')
+export default function PeoplePage({
+  params: { slug }
+}: PageProps<{ slug: string }>) {
+  return <ProfilePage segment="people" slug={slug} />
+}
