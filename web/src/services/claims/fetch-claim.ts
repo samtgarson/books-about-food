6e361f557d079664d3bd0590dba09cfeb0cae550
Sam Garson <samtgarson@gmail.com>
2023-09-17 22:45:46 +0100

    Fix TS error with claims

--- web/src/services/claims/fetch-claim.ts
+++ web/src/services/claims/fetch-claim.ts
@@ -3,41 +3,21 @@ import { Service } from 'src/utils/service'
 import { z } from 'zod'
 
 export const fetchClaim = new Service(
-  z
-    .object({
-      profileId: z.string(),
-      publisherId: z.never().optional()
-    })
-    .or(
-      z.object({
-        publisherId: z.string(),
-        profileId: z.never().optional()
-      })
-    ),
-  async ({ profileId, publisherId } = {}, user) => {
+  z.object({
+    profileId: z.string()
+  }),
+  async ({ profileId } = {}, user) => {
     if (!user) return
-    if (!profileId && !publisherId) return
     const userId = user.id
 
-    if (profileId)
-      return prisma.claim.findUnique({
-        where: {
-          userId_profileId: {
-            userId,
-            profileId
-          }
+    return prisma.claim.findUnique({
+      where: {
+        userId_profileId: {
+          userId,
+          profileId
         }
-      })
-
-    if (publisherId)
-      return prisma.claim.findUnique({
-        where: {
-          userId_publisherId: {
-            userId,
-            publisherId
-          }
-        }
-      })
+      }
+    })
   },
   {
     cache: {
