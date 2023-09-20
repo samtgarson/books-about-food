209019d85d47cd28d385afa78c67312b62c51208
Sam Garson <samtgarson@gmail.com>
2023-09-20 11:55:33 +0100

    Cache service calls within request cycle

--- web/src/utils/service.ts
+++ web/src/utils/service.ts
@@ -1,5 +1,8 @@
-import { User } from 'database'
+import { authOptions } from 'app/api/auth/[...nextauth]/route'
+import prisma, { User } from 'database'
+import { getServerSession } from 'next-auth'
 import { z } from 'zod'
+import { cache } from 'react'
 
 export type RequestMeta = {
   cache?: {
@@ -10,17 +13,33 @@ export type RequestMeta = {
 }
 
 export class Service<Input extends z.ZodTypeAny, Return> {
+  private _call: (input?: z.infer<Input>, user?: User | null) => Promise<Return>
   constructor(
     public input: Input,
-    public call: (
-      input?: z.infer<Input>,
-      user?: User | null
-    ) => Promise<Return>,
+    call: (input?: z.infer<Input>, user?: User | null) => Promise<Return>,
     public requestMeta: RequestMeta = {}
-  ) {}
+  ) {
+    this._call = cache(call)
+  }
 
   public parseAndCall(input: unknown | z.infer<Input>, user?: User | null) {
     const parsed = this.input.parse(input)
     return this.call(parsed, user)
+  }
+
+  public async call(input?: z.infer<Input>, user?: User | null) {
+    const u = user || (await this.getUser())
+
+    return this._call(input, u)
+  }
+
+  private async getUser() {
+    const session = await getServerSession(authOptions)
+
+    if (!session?.user.id) return null
+
+    return prisma.user.findUnique({
+      where: { id: session.user.id }
+    })
   }
 }
