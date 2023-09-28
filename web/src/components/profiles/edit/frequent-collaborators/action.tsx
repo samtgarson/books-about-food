8c411c08d5ac20ed5254fcead271d9d1ffa68447
Sam Garson <samtgarson@gmail.com>
2023-09-28 12:19:54 +0100

    Install knip and delete some unused code

--- web/src/components/profiles/edit/frequent-collaborators/action.tsx
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { fetchFrequentCollaborators } from 'src/services/books/fetch-frequent-collaborators'
-import { stringify } from 'src/utils/superjson'
-
-export const fetch = async (slug: string) =>
-  stringify(await fetchFrequentCollaborators.call({ slug }))
