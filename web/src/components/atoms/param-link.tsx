2311b127d679697777ad4492cd950007d3c360e8
Sam Garson <samtgarson@gmail.com>
2023-11-04 23:59:13 +0000

    Try unwrapping useSearchParams from Suspense

--- web/src/components/atoms/param-link.tsx
+++ web/src/components/atoms/param-link.tsx
@@ -1,15 +1,14 @@
 'use client'
 
 import { usePathname, useSearchParams } from 'next/navigation'
-import { Children, FC, Suspense, cloneElement } from 'react'
+import { Children, FC, cloneElement } from 'react'
 import { mergeParams } from 'src/utils/url-helpers'
-import { Loader } from './loader'
 
 type ParamLinkParams = Record<string, unknown | undefined | null> & {
   children: JSX.Element
 }
 
-const ParamLinkContent: FC<ParamLinkParams> = ({ children, ...props }) => {
+export const ParamLink: FC<ParamLinkParams> = ({ children, ...props }) => {
   const searchParams = useSearchParams() || new URLSearchParams()
   const pathName = usePathname() || ''
   const params = new URLSearchParams(Object.fromEntries(searchParams.entries()))
@@ -18,9 +17,3 @@ const ParamLinkContent: FC<ParamLinkParams> = ({ children, ...props }) => {
     href: mergeParams(props, pathName, params)
   })
 }
-
-export const ParamLink: FC<ParamLinkParams> = (props) => (
-  <Suspense fallback={<Loader />}>
-    <ParamLinkContent {...props} />
-  </Suspense>
-)
