f1a12f59cff419500c7f7004a4ebef3e1d90f1d6
Sam Garson <samtgarson@gmail.com>
2023-11-17 21:50:56 +0000

    Update copy on oauth connect error

--- web/src/app/auth/sign-in/page.tsx
+++ web/src/app/auth/sign-in/page.tsx
@@ -17,7 +17,7 @@ const errors: Record<SignInErrorTypes, string> = {
   EmailCreateAccount: 'Try signing in with a different account.',
   Callback: 'Try signing in with a different account.',
   OAuthAccountNotLinked:
-    'To confirm your identity, sign in with the same account you used originally.',
+    'To confirm your identity, sign in with the same account you used originally and connect your new account in the settings page.',
   EmailSignin: 'The e-mail could not be sent.',
   CredentialsSignin:
     'Sign in failed. Check the details you provided are correct.',
