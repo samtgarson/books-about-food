1b81acca541151edfabccc7f684af3b870a770f7
Sam Garson <samtgarson@gmail.com>
2023-12-04 14:46:28 +0000

    Add sentry DSN to config files using env var

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -5,6 +5,8 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
