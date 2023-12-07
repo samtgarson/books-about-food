8d2fff909d8fe4d01e449cad1bf1dd7f86c8d5be
Sam Garson <samtgarson@gmail.com>
2023-12-07 15:51:47 +0000

    Run sentry on the Admin app

--- admin/lib/index.ts
+++ admin/lib/index.ts
@@ -1,7 +1,12 @@
 import cors from '@koa/cors'
+import * as Sentry from '@sentry/node'
 import Koa from 'koa'
 import { agent } from './agent'
 import { apiRouter } from './api'
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN
+})
 
 const app = new Koa()
 
@@ -14,6 +19,13 @@ app.use(
 )
 agent.mountOnKoa(app)
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods())
+
+app.on('error', (err, ctx) => {
+  Sentry.withScope((scope) => {
+    scope.setSDKProcessingMetadata({ request: ctx.request })
+    Sentry.captureException(err)
+  })
+})
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5001
 app.listen(port, () => {
