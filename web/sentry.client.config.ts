aa356e62aa4f170ddead0940a7cce52b9bd47f3d
Sam Garson <samtgarson@gmail.com>
2023-12-14 10:14:41 +0000

    [WEB] Reduce sentry replays

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -13,11 +13,11 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: process.env.NODE_ENV === 'production' ? 1 : 0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
