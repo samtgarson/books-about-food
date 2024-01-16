953edcbba4c60e842b75425efd2a6f4433fc77e6
Sam Garson <samtgarson@gmail.com>
2024-01-16 10:39:25 +0000

    [WEB] Disable performance monitoring with sentry

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -1,5 +1,3 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
@@ -7,19 +5,14 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.25 : 0,
+  tracesSampleRate: 0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   replaysOnErrorSampleRate: process.env.NODE_ENV === 'production' ? 1 : 0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
     new Sentry.Replay({
       // Additional Replay configuration goes in here, for example:
--- web/sentry.server.config.ts
+++ web/sentry.server.config.ts
@@ -1,5 +1,3 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import prisma from '@books-about-food/database'
@@ -7,10 +5,8 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.25 : 0,
+  tracesSampleRate: 0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   integrations: [new Sentry.Integrations.Prisma({ client: prisma })]
