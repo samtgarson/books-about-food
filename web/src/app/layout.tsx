91e4c2ab2ff592957e0386b2ef34454e8e26fe31
Sam Garson <samtgarson@gmail.com>
2024-03-29 15:59:24 +0000

    [WEB] Fix issue with older versions of chrome `globalThis`

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -19,6 +19,14 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" className={fontClassname}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            // defines globalThis in environments where it's not
+            __html: `!function(t){function e(){var e=this||self;e.globalThis=e,delete t.prototype._T_}"object"!=typeof globalThis&&(this?e():(t.defineProperty(t.prototype,"_T_",{configurable:!0,get:e}),_T_))}(Object);`
+          }}
+        />
+      </head>
       <body className="relative flex min-h-screen flex-col transition-colors duration-700">
         <Sentry.ErrorBoundary fallback={<div>FOO</div>}>
           <Fathom />
