f158292a19a16f5b5c25c43f38fadf16edf70492
Sam Garson <samtgarson@gmail.com>
2024-04-19 17:45:00 +0100

    Patch prisma for edge runtime

--- web/src/components/admin/import/action.ts
+++ web/src/components/admin/import/action.ts
@@ -6,9 +6,15 @@ import { ProcessBookImportInput } from '@books-about-food/core/services/import/i
 // TODO: Move import to admin API
 
 export async function parseCsv(_input: ImportBooksInput) {
+  return { success: true, data: [], errors: [] }
   // return parseAndCall(importBooks, input)
 }
 
 export async function process(_input: ProcessBookImportInput) {
+  return {
+    success: true,
+    data: [] as string[],
+    errors: [{ field: '', message: '' }]
+  }
   // return parseAndCall(processBookImport, input)
 }
