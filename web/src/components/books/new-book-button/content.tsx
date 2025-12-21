ef97ef3155046ed4c3ee86f998e76ee7953f28ca
Sam Garson <samtgarson@gmail.com>
2025-12-21 18:54:30 +0000

    Go straight to book edit after entering title

--- web/src/components/books/new-book-button/content.tsx
+++ web/src/components/books/new-book-button/content.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { TitleSelectChangeAttrs } from 'src/components/edit/books/forms/title/action'
-import { TitleFormContent } from 'src/components/edit/books/forms/title/form-content'
 import { TitleSelect } from 'src/components/edit/books/forms/title/title-select'
 import { Form } from 'src/components/form'
 import { Submit } from 'src/components/form/submit'
@@ -21,9 +20,28 @@ export function NewBookForm() {
       <TitleSelect onChange={setValues} />
       <input type="hidden" name="title" value={book.title ?? ''} />
       <input type="hidden" name="googleBooksId" value={googleBooksId ?? ''} />
-      <input type="hidden" name="cover" value={cover} />
-      {values?.title && <TitleFormContent book={book} />}
-      {values?.title && <Submit className="w-full">Save & continue</Submit>}
+      {values?.cover && <input type="hidden" name="cover" value={cover} />}
+      {values?.subtitle && (
+        <input type="hidden" name="subtitle" value={book.subtitle} />
+      )}
+      {values?.authors && values.authors.length > 0 && (
+        <input
+          type="hidden"
+          name="authorIds"
+          value={values.authors.map((author) => author.id).join(',')}
+        />
+      )}
+      {values?.tags && values.tags.length > 0 && (
+        <input
+          type="hidden"
+          name="tags"
+          value={values.tags.map((tag) => tag.slug).join(',')}
+        />
+      )}
+      {/*{values?.title && <TitleFormContent book={book} />}*/}
+      <Submit className="w-full" disabled={!values?.title}>
+        Save & continue
+      </Submit>
     </Form>
   )
 }
